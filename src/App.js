ba5d8de5fa2433d41e5f8e8b0e7ffb6e723b185f
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2022-01-15 22:42:38 +0530

    Boilerplate for React-Electron project made ready

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // The Electron.js configuration is in
+  // the 'public/main.js' file. The starting scripts
+  // are written in package.json.
   return (
     <div className="App">
       <header className="App-header">
